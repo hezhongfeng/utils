cc3cdc469ace8fd4994d21baa2358e5404d8aa45
hezhongfeng <287645587@qq.com>
2024-04-09 09:38:19 +0800

    📃 docs(添加一些 log 和 说明):

--- network-request-concurrency-control/test.js
+++ network-request-concurrency-control/test.js
@@ -1,13 +1,9 @@
 import { multiRequest } from './utils.js';
 
 const run = async (urlLength, maxNum) => {
-  console.time(`网络请求并发测试，url 数量 ${urlLength}，并发数 ${maxNum}`);
-
   const urls = new Array(urlLength).fill('https://www.baidu.com/');
 
   await multiRequest(urls, maxNum);
-
-  console.timeEnd(`网络请求并发测试，url 数量 ${urlLength}，并发数 ${maxNum}`);
 };
 
-run(500, 30);
+run(1000, 20);
--- network-request-concurrency-control/utils.js
+++ network-request-concurrency-control/utils.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const multiRequest = async (urls, maxNum) => {
   return new Promise((resolve, reject) => {
+    const length = urls.length;
+    console.time(`网络请求并发测试，url 数量 ${length}，并发数 ${maxNum}，一共花费的时间是：`);
+    // 当前的并发数
     let inFlight = 0;
+    // 任务索引
     let index = 0;
 
     const run = () => {
@@ -15,10 +19,12 @@ const multiRequest = async (urls, maxNum) => {
 
     const makeRequest = i => {
       const url = urls[i];
+      console.time(`这是第 ${i + 1} '个请求，花费的时间是：'`);
       axios
         .get(url)
         .then(() => {
-          console.log('请求成功，这是第 ' + (i + 1) + '个');
+          // console.log(`请求成功，这是第 ${i + 1} 个请求`);
+          console.timeEnd(`这是第 ${i + 1} '个请求，花费的时间是：'`);
         })
         .catch(() => {
           reject();
@@ -28,6 +34,7 @@ const multiRequest = async (urls, maxNum) => {
           run();
           if (inFlight === 0) {
             console.log('请求完毕');
+            console.timeEnd(`网络请求并发测试，url 数量 ${length}，并发数 ${maxNum}，一共花费的时间是：`);
             resolve();
           }
         });
